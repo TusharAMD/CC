598b9d08f529c2a68b4fa99552d34881ce995a22
Tushar <tusharvamanamdoskar@gmail.com>
2022-04-13 09:44:51 +0530

    7

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,11 @@
 const About = () => {
 	return (
-	<div>
-In this project we are building a Art Exhibition Website.
-Sometimes you want to show your creativity to the world but you don't have a perfect platform for the same.
-Yes there do exists websites like Instagram but its more of a general social app and our project would niche website specifically for art lovers.</div>)
+	<div className="aboutbox">
+	<div className="about">
+	In this project we are building a Art Exhibition Website.
+	Sometimes you want to show your creativity to the world but you don't have a perfect platform for the same.
+	Yes there do exists websites like Instagram but its more of a general social app and our project would niche website specifically for art lovers.</div>
+	</div>
+)
 }
 export default About
